247c1ae9c24f822731fee5e2c3210ccba52e33d8
Melanie <melita230604@gmail.com>
2023-05-18 20:48:22 +0200

    siiii

---
+++ src/api/viajesmilano.js
@@ -0,0 +1,24 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'X-RapidAPI-Key': 'f3acb23f25mshe6453e96f325d96p1d82b0jsn40800864f642',
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
